37c61ffbec18c9ef31ff942522e2a85508e2b31b
vereseg <me@vereseg.com>
2025-02-23 02:04:31 -0600

    add programs file

---
+++ programs.ps1
@@ -0,0 +1,27 @@
+#basic programs
+winget install -e --id VideoLAN.VLC -i
+winget install -e --id Giorgiotani.Peazip -i
+
+#extra programs
+winget install -e --id Microsoft.PowerToys -i
+
+#sysinternals programs
+winget install -e --id Microsoft.Sysinternals.TCPView
+winget install -e --id Microsoft.Sysinternals.ProcessExplorer
+winget install -e --id Microsoft.Sysinternals.ProcessMonitor
+winget install -e --id Microsoft.Sysinternals.Autoruns
+
+#basic dev tools
+winget install -e --id Git.Git -i
+winget install -e --id Microsoft.VisualStudioCode -i
+winget install -e --id Notepad++.Notepad++ -i
+
+#extra dev tools
+winget install -e --id StephanDilly.gitui
+
+#web dev tools
+winget install -e --id OpenJS.NodeJS -i
+winget install -e --id pnpm.pnpm -i
+
+#c dev tools
+winget install -e --id Microsoft.VisualStudio.2022.Community -i
